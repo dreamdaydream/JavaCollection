402ab348d8d25c1525e8b89da393f3a98522e034
zero <fll_005@163.com>
2019-03-24 21:31:24 +0800

    solve about map

--- src/main/java/com/thoughtworks/collection/MyMap.java
+++ src/main/java/com/thoughtworks/collection/MyMap.java
@@ -3,7 +3,9 @@ package com.thoughtworks.collection;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MyMap {
 
@@ -17,22 +19,45 @@ public class MyMap {
     }
 
     public List<Integer> getTriple() {
-        throw new NotImplementedException();
+
+        List<Integer> result = array.stream().map(item -> item*3).collect(Collectors.toList());
+        return result;
     }
 
     public List<String> mapLetter() {
-        throw new NotImplementedException();
+        List<String> result = array.stream().map(item -> letterList.get(item-1)).collect(Collectors.toList());
+        return result;
     }
 
     public List<String> mapLetters() {
-        throw new NotImplementedException();
+
+        List<String> result = array.stream().map(item ->getLetter(item)).collect(Collectors.toList());
+        return result;
+    }
+
+    public String getLetter(int num){
+        //要改进
+        String letters = "";
+        while(num/27!=0){
+            int point = num%26;
+            if (point==0){
+                point=26;
+                num=num-1;
+            }
+            letters = letterList.get(point-1)+letters;
+            num=num/26;
+        }
+        letters=letterList.get(num-1)+letters;
+        return letters;
     }
 
     public List<Integer> sortFromBig() {
-        throw new NotImplementedException();
+        List<Integer> reuslt = array.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+        return reuslt;
     }
 
     public List<Integer> sortFromSmall() {
-        throw new NotImplementedException();
+        List<Integer> reuslt = array.stream().sorted().collect(Collectors.toList());
+        return reuslt;
     }
 }
