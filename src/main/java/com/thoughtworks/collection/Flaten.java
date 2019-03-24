efcee60a9323e8d8451edd3c8b1a269a7c02ce22
zero <fll_005@163.com>
2019-03-24 22:43:32 +0800

    finish the inverseReduce and flaten

--- src/main/java/com/thoughtworks/collection/Flaten.java
+++ src/main/java/com/thoughtworks/collection/Flaten.java
@@ -2,6 +2,8 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Flaten {
@@ -12,10 +14,33 @@ public class Flaten {
     }
 
     public List<Integer> transformToOneDimesional() {
-        throw new NotImplementedException();
+        int length=0;
+        for(Integer[] item:array){
+            length=length+item.length;
+        }
+        Integer[] result =new Integer[length];
+        int index = 0;
+        for(Integer[] item : array){
+            for(int items :item){
+                result[index++]=items;
+            }
+        }
+        return Arrays.asList(result);
     }
 
     public List<Integer> transformToUnrepeatedOneDimesional() {
-        throw new NotImplementedException();
+        int length=0;
+        for(Integer[] item:array){
+            length=length+item.length;
+        }
+        List<Integer> result =new ArrayList<>();
+        for(Integer[] item : array){
+            for(int items :item){
+                if (!result.contains(items)) {
+                    result.add(items);
+                }
+            }
+        }
+        return result ;
     }
 }
